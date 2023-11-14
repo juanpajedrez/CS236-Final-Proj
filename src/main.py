70dd76ecf27af5ba2e500a75d6b94aa9b10d5219
juanpajedrez <jptriana.martinez@gmail.com>
2023-11-14 10:16:38 -0500

    B1_DataLoading: Addition of utils folder and main.py for proper setup

---
+++ src/main.py
@@ -0,0 +1,8 @@
+'''
+Date: 2023-11-14
+Authors: Kuniaki Iwanami, Masa Someha, Juan Pablo Triana Martinez
+Project: CS236 Final Project, VAE for X-rays images.
+'''
+
+if __name__ == "main":
+    ...
